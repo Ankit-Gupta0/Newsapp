43bf2790fe14c468454a3056356302b996f6563a
harshverma036 <harshverma0362@gmail.com>
2021-11-23 15:58:00 +0530

    url fixes done

--- src/components/News.js
+++ src/components/News.js
@@ -29,7 +29,7 @@ export class News extends Component {
         console.log(parsedData);    
         this.setState({
             page: this.state.page - 1,
-            articles: parsedData.articles
+            articles:  parsedData.articles
         })
 
     }
@@ -39,10 +39,10 @@ export class News extends Component {
         if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
 
         }else{
-            let url =`https://newsapi.org/v2/top-headlines?country=in&apiKey=e33da568b72b4bb4bcf80b7619a50ad7&page=&{this.state.page + 1}&pageSize=20`;
+            let url =`https://newsapi.org/v2/top-headlines?country=in&apiKey=e33da568b72b4bb4bcf80b7619a50ad7&page=${this.state.page + 1}&pageSize=20`;
             let data = await fetch(url);
             let parsedData = await data.json();
-            console.log(parsedData);    
+            console.log(parsedData, "this is parse data");    
             this.setState({
                 page: this.state.page + 1,
                 articles: parsedData.articles
@@ -56,7 +56,7 @@ export class News extends Component {
             <div className="container my-3">
                 <h2>NewsMonkey - Top Headline</h2>
                 <div className="row">
-                {this.state.articles.map((element)=>{
+                {this.state.articles && this.state.articles.map((element)=>{
                     return <div className="col-md-4" key={element.url}>
                         <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl ={element.urlToImage} newsUrl={element.url} />
                     </div>
